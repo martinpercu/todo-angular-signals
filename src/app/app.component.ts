cdc37c769b9f0f05bd39d18ab75040e0c61b9aa5
Martin Mendez <martinpercu@gmail.com>
2023-12-07 12:24:42 -0500

    adding basic task list with signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Task } from './../models/task.model';
@@ -13,5 +13,14 @@ import { Task } from './../models/task.model';
 export class AppComponent {
 
   title = 'todoapp';
+
+  tasks = signal([
+    'do something 1',
+    'do something 2',
+    'do something 3',
+    'do something 4',
+    'do something 5'
+  ]);
+
 
 }
